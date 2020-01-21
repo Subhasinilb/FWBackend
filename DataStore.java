c48914ab9d70fbc80b2f158c03d815c1c51894bb
Subhasinilb <59996375+Subhasinilb@users.noreply.github.com>
2020-01-21 14:45:06 +0530

    Update DataStore.java

--- DataStore.java
+++ DataStore.java
@@ -1,6 +1,17 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import org.json.JSONException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
 public static void main(String args[])
 {
 	String Key = new String();
+	
+	System.out.print("\n1.Create Key-Value Data Store\n2.Read from Data Store \n3.Delete Key-Value pair \n4.Exit\nEnter your option:");
 }
 
 
@@ -8,9 +19,10 @@ public static void main(String args[])
 
 public static void create(String Key, JSONObject Value)
 {
-					JSONObject obj = new JSONObject();
-					obj.put("key", Key);
-					obj.put("value", Value);
+	JSONObject obj = new JSONObject();
+	
+	obj.put("key", Key);
+	obj.put("value", Value);
 }
 
 
