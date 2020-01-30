de3112b89ea2611b5736b81a13ce3755017de1a7
Subhasinilb <59996375+Subhasinilb@users.noreply.github.com>
2020-01-30 17:49:21 +0530

    Update and rename DataStore.java to KeyValueDataStore.java

--- KeyValueDataStore.java
+++ KeyValueDataStore.java
@@ -7,12 +7,13 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 
-public static void main(String args[])
+public class Backend
 {
-	String Key = new String();
-	
-	System.out.print("\n1.Create Key-Value Data Store\n2.Read from Data Store \n3.Delete Key-Value pair \n4.Exit\nEnter your option:");
-}
+	public static void main(String args[]) throws JSONException
+	{
+		system.out.print("\n1.Create Key-Value Data Store\n2.Read from Data Store \n3.Delete Key-Value pair \n4.Exit\nEnter your option:");
+		String Key = new String();
+		
 
 
 // create a data store with key and value
@@ -29,4 +30,18 @@ public static void create(String Key, JSONObject Value)
 // read the key and check for deuplicates
 
 public static void read(String key)
-					
+{
+	JSONObject obj = new JSONObject();
+	
+	obj.get("key");
+}
+
+//Delete Key-value pair
+
+public static void delete(String Key, JSONObject Value)
+{
+	JSONObject obj = new JSONObject();
+	
+	obj.delete("key");
+}
+		
